cecd6c804708aa8e90b8fb4c71c503f3d4bfd9cf
XxMar1an0xX <FrodoEpifanio@protonmail.com>
2025-06-11 11:35:25 -0300

    maquina virtual inicio

---
+++ main/modulos/nixconfig/VMs.nix
@@ -0,0 +1,8 @@
+{...}: {
+  programs.virt-manager.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+  };
+  virtualisation.spiceUSBRedirection.enable = true;
+  users.users.portable.extraGroups = ["libvirtd"];
+}
