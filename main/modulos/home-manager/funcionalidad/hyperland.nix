e837a8d8ebf43888b9c16e06dd74bac70529170f
XxMar1an0xX <FrodoEpifanio@protonmail.com>
2025-06-15 15:25:19 -0300

    muchos keybinds

--- main/modulos/home-manager/funcionalidad/hyperland.nix
+++ main/modulos/home-manager/funcionalidad/hyperland.nix
@@ -8,10 +8,13 @@
   #NOTE: apps que ayudan mucho
   home.packages = with pkgs; [
     wofi
+    hyprsysteminfo
   ];
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
+    systemd.enable = false;
+    # systemd.variables = ["--all"];
     settings = {
       # Keywords hyprland
       "$terminal" = "kitty";
@@ -23,6 +26,7 @@
       exec-once = [
         "$terminal"
         "$browser"
+        "systemctl --user start hyprpolkitagent"
       ];
 
       # Variables de entorno (Enviroment variables)
@@ -171,15 +175,37 @@
         "$mainMod, S, togglespecialworkspace, magic"
         "$mainMod SHIFT, S, movetoworkspace, special:magic"
 
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, U, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
-        "$mainMod, I, workspace, e-1"
+        # "$mainMod, mouse_down, workspace, e+1"
+        # "$mainMod, U, workspace, e+1"
+        # "$mainMod, mouse_up, workspace, e-1"
+        # "$mainMod, I, workspace, e-1"
 
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
         "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, resizewindow"
+        "$mainMod, mouse:273, resizeactive"
 
         "ALT, F4, forcekillactive"
+        "$mainMod, X, exec, shutdown -h now"
+        "$mainMod, N, exec, $terminal -e nvim Documentos/GitHub/nixos/main/"
       ];
       binds = {
         drag_threshold = "5";
