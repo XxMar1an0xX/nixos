ce1a784023dc5e79eb493ed178403ae586ed7347
XxMar1an0xX <FrodoEpifanio@protonmail.com>
2025-06-16 10:23:45 -0300

    quizas estos nerdfont son solo los iconos

--- main/hosts/nix-on-droid/home.nix
+++ main/hosts/nix-on-droid/home.nix
@@ -8,6 +8,12 @@
   # Read the changelog before changing this value
   home.stateVersion = "24.05";
   home.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.noto
+    nerd-fonts.hack
+    nerd-fonts.ubuntu
+    nerd-fonts.mplus
   ];
 
   imports = [
