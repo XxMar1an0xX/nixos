2636aa0449456105ae5007b0aa566f4075cffa6b
XxMar1an0xX <FrodoEpifanio@protonmail.com>
2025-06-24 19:22:14 -0300

    usuario fix

---
+++ main/modulos/nixconfig/usuario-portable.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+  users.users = {
+    portable = {
+      isNormalUser = true;
+      description = "portable mariano";
+      extraGroups = ["networkmanager" "wheel"];
+      hashedPassword = "$y$j9T$FBd2m8UD9gZQFSBLZlk.N0$URtutPSPulECzvPHX5.Q2quC4VFSL.nYN7/jCj5Kky6";
+    };
+  };
+}
