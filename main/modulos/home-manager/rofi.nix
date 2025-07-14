8dc525cf0c52c26a705bc01f1735834d715135f5
XxMar1an0xX <frodoepifanio@protonmail.com>
2025-07-14 15:20:29 -0300

    search placeholder rofi

--- main/modulos/home-manager/rofi.nix
+++ main/modulos/home-manager/rofi.nix
@@ -1,16 +1,20 @@
-{pkgs, lib, config, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   programs.rofi = {
     enable = true;
     cycle = true;
     extraConfig = {
-
-modi = ["drun" "window" "run"];
+      modi = ["drun" "window" "run"];
       icon-theme = "Papyrus-Dark";
       show-icons = true;
       drun-display-format = "{icon} {name}";
-    display-drun = " ";
-    display-run = " ";
-    display-window = " ";
+      display-drun = " ";
+      display-run = " ";
+      display-window = " ";
 
       kb-row-up = "Up,Control+k";
       kb-row-down = "Down,Control+j";
@@ -27,135 +31,140 @@ modi = ["drun" "window" "run"];
   };
   home.file.".local/share/rofi/themes/rofi-custom.rasi".text = let
     stylix = config.lib.stylix.colors;
-  in ''
-      * {
-    bg: #${stylix.base01};
-    hv: #${stylix.base04}; 
-    primary: #${stylix.base0D}; 
-    ug: #${stylix.base03};
-    font: "Monospace 11";
-    background-color: @bg;
-    //dark: @bg;
-    border: 0px;
-    kl: #${stylix.base06};
-    black: #${stylix.base00};
+  in
+    /**/
+    ''
+            * {
+          bg: #${stylix.base01};
+          hv: #${stylix.base04};
+          primary: #${stylix.base0D};
+          ug: #${stylix.base03};
+          font: "Monospace 11";
+          background-color: @bg;
+          //dark: @bg;
+          border: 0px;
+          kl: #${stylix.base06};
+          black: #${stylix.base00};
 
-    transparent: rgba(${stylix.base02-rgb-r},${stylix.base02-rgb-g},${stylix.base02-rgb-b},0);
-}
+          transparent: rgba(${stylix.base02-rgb-r},${stylix.base02-rgb-g},${stylix.base02-rgb-b},0);
+      }
 
-window {
-    width: 700;
-    /*since line wont work with height, i comment it out 
-    if you rather control the size via height
-    just comment it out */
-    //height: 500;
+      window {
+          width: 700;
+          /*since line wont work with height, i comment it out
+          if you rather control the size via height
+          just comment it out */
+          //height: 500;
 
-    orientation: horizontal;
-    location: center;
-    anchor: center;
-    transparency: "screenshot";
-    border-color: @transparent;   
-    border: 0px;
-    border-radius: 6px;
-    spacing: 0;
-    children: [ mainbox ];
-}
 
-mainbox {
-    spacing: 0;
-    children: [ inputbar, message, listview ];
-}
+          orientation: horizontal;
+          location: center;
+          anchor: center;
+          transparency: "screenshot";
+          border-color: @transparent;
+          border: 0px;
+          border-radius: 6px;
+          spacing: 0;
+          children: [ mainbox ];
+      }
 
-inputbar {
-    color: @kl;
-    padding: 11px;
-    border: 3px 3px 2px 3px;
-   border-color: @primary;
-    border-radius: 6px 6px 0px 0px;
-}
+      mainbox {
+          spacing: 0;
+          children: [ inputbar, message, listview ];
+      }
 
-message {
-    padding: 0;
-    border-color: @primary;
-    border: 0px 1px 1px 1px;
-}
+      inputbar {
+          color: @kl;
+          padding: 11px;
+          border: 3px 3px 2px 3px;
+         border-color: @primary;
+          border-radius: 6px 6px 0px 0px;
+      }
 
-entry, prompt, case-indicator {
-    text-font: inherit;
-    text-color: inherit;
-}
+      message {
+          padding: 0;
+          border-color: @primary;
+          border: 0px 1px 1px 1px;
+      }
 
-entry {
-    cursor: pointer;
-}
+      entry, prompt, case-indicator {
+          text-font: inherit;
+          text-color: inherit;
+      }
 
-prompt {
-    margin: 0px 5px 0px 0px;
-}
+      entry {
+          cursor: pointer;
+          placeholder:        "Search";
+      }
 
-listview {
-    layout: vertical;
-    //spacing: 5px;
-    padding: 8px;
-    lines: 7;
-    columns: 1;
-    border: 0px 3px 3px 3px; 
-    border-radius: 0px 0px 6px 6px;
-    border-color: @primary;
-    dynamic: false;
-}
+      prompt {
+          margin: 0px 5px 0px 0px;
+      enabled:     true;
+      }
 
-element {
-    padding: 2px;
-    vertical-align: 1;
-   color: @kl;
-    font: inherit;
-}
+      listview {
+          layout: vertical;
+          //spacing: 5px;
+          padding: 8px;
+          lines: 7;
+          columns: 1;
+          border: 0px 3px 3px 3px;
+          border-radius: 0px 0px 6px 6px;
+          border-color: @primary;
+          //dynamic: true;
+      }
 
-element-text {
-    background-color: inherit;
-    text-color: inherit;
-    vertical-align: 0.5;
-}
+      element {
+          padding: 2px;
+          vertical-align: 1;
+         color: @kl;
+          font: inherit;
+      }
 
-element selected.normal {
-    color: @black;
-    background-color: @hv;
-}
+      element-text {
+          background-color: inherit;
+          text-color: inherit;
+          vertical-align: 0.5;
+      }
 
-element normal active {
-    background-color: @hv;
-   color: @black;
-}
+      element selected.normal {
+          color: @black;
+          background-color: @hv;
+      }
 
-element-icon {
-    background-color: inherit;
-    text-color: inherit;
-    size: 2.5em;
-}
+      element normal active {
+          background-color: @hv;
+         color: @black;
+      }
 
-element normal urgent {
-    background-color: @primary;
-}
+      element-icon {
+          background-color: inherit;
+          text-color: inherit;
+          size: 2.5em;
+      }
 
-element selected active {
-    background: @hv;
-    foreground: @bg;
-}
+      element normal urgent {
+          background-color: @primary;
+      }
 
-button {
-    padding: 6px;
-    color: @primary;
-    horizonatal-align: 0.5;
+      element selected active {
+          background: @hv;
+          foreground: @bg;
+      }
 
-    border: 2px 0px 2px 2px;
-    border-radius: 4px 0px 0px 4px;
-    border-color: @primary;
-}
+      button {
+          padding: 6px;
+          color: @primary;
+          horizonatal-align: 0.5;
 
-button selected normal {
-    border: 2px 0px 2px 2px;
-    border-color: @primary;
-}
+          border: 2px 0px 2px 2px;
+          border-radius: 4px 0px 0px 4px;
+          border-color: @primary;
+      }
+
+      button selected normal {
+          border: 2px 0px 2px 2px;
+          border-color: @primary;
+      }
     '';
 }
