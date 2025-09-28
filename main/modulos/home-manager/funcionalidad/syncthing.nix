001765891ae62340fcc185b22cffa9b7f7c480f5
XxMar1an0xX <frodoepifanio@protonmail.com>
2025-09-28 12:25:54 -0300

    syncthing declarado

--- main/modulos/home-manager/funcionalidad/syncthing.nix
+++ main/modulos/home-manager/funcionalidad/syncthing.nix
@@ -1,10 +1,23 @@
 {...}: {
   services.syncthing = {
-    enable = false;
+    enable = true;
     tray.enable = true;
+    # openDefaultPorts = true;
     settings = {
-      folders = {
+      devices = {
+        "Celular" = {
+          id = "L5OITQQ-AHJXITF-BHYMNOR-JFOP4ZT-2OPRTEQ-FC4DGVE-FFXWOSJ-6JOLTA5";
+        };
       };
+      folders = {
+        "Syncron" = {
+          path = "/home/ruiz/Documentos/Syncron";
+          devices = ["Celular"];
+        };
+      };
+      # options = {
+      #   urAccepted = "-1";
+      # };
     };
   };
 }
