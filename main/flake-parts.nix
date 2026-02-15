f2b53bde446a5d917ae4a2183e56bcf18301d497
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-02-15 20:04:26 -0300

    flake part init

---
+++ main/flake-parts.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+      ];
+      systems = ["x86_64-linux" "aarch64-linux"];
+      persystem = {
+      };
+      flake = {
+      };
+    };
+}
