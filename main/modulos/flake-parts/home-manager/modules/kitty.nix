dae2d68079dd6a38703a10c220675bd1431d5f68
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-04-19 18:45:34 -0300

    akjdjkhskjahdasska

---
+++ main/modulos/flake-parts/home-manager/modules/kitty.nix
@@ -0,0 +1,25 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.homeModules.kitty = {
+    programs.kitty = {
+      enable = true;
+      extraConfig =
+        /*
+        conf
+        */
+        ''
+          cursor_trail 1
+          confirm_os_window_close 0
+          map ctrl+c copy_or_interrupt
+          map ctrl+v paste_from_clipboard
+        '';
+
+      settings = {
+        enable_audio_bell = false;
+      };
+    };
+  };
+}
