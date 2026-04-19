5dd4a52e3b243388c93d9650ccb8591c7cb03702
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-04-19 19:24:40 -0300

    aksjdhskja

---
+++ main/modulos/flake-parts/home-manager/homeDroid.nix
@@ -0,0 +1,69 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.homeModules.homeDroid = {
+    pkgs,
+    inputs,
+    rust-overlay,
+    # nvf,
+    ...
+  }: {
+    imports = [
+      self.homeModules.fastfetch
+      self.homeModules.github
+      self.homeModules.config-cortas
+      # ./../../modulos/home-manager/stylix-hm.nix
+    ];
+
+    nixpkgs.overlays = [
+      rust-overlay.overlays.default
+    ];
+    # Read the changelog before changing this value
+    home.stateVersion = "24.05";
+    home.packages = with pkgs; [
+      nerd-fonts.symbols-only
+      mp3gain
+      unzip
+      rust-bin.stable.latest.default
+    ];
+
+    fonts.fontconfig = {
+      enable = true;
+      # defaultFonts.emoji = pkgs.nerd-fonts.symbols-only;
+    };
+
+    nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+    programs.zsh = {
+      enable = true;
+    };
+
+    programs.home-manager.enable = true;
+    # insert home-manager config
+    home.shellAliases = {
+      ds = "nix-on-droid switch --flake $HOME/nixos/main/";
+      nc = "cd $HOME/nixos/main/ && git pull --no-edit && vi .";
+      ".." = "cd ..";
+      x = "exit";
+    };
+    # systemd.user.services.fastfetch = {
+    #   Unit = {
+    #     Description = "systemd service for automatic mounting";
+    #     # after = ["network.target.service"];
+    #   };
+    #   Install = {
+    #     WantedBy = ["multi-user.target"];
+    #   };
+    #   Service = {
+    #     Type = "simple";
+    #     # User = "root";
+    #     # Group = "root";
+    #     # PermissionsStartOnly = true;
+    #     ExecStart = "${pkgs.writeShellScript "ff" ''
+    #       fastfetch
+    #     ''}";
+    #   };
+    # };
+  };
+}
