dde400d59dd43ad345354e6e615a3ec54dc46f91
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-04-23 16:56:19 -0300

    ashdjkd

---
+++ main/modulos/flake-parts/custom-pkgs/NVF/default.nix
@@ -0,0 +1,22 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  perSystem = {pkgs, ...}: {
+    packages.NVF =
+      (inputs.nvf.lib.neovimConfiguration {
+        modules = [
+          self.modules.nvf.baseConfig
+          # {
+          #   _module.args = let
+          #     hola = "${self.packages.aarch64-linux.arduino-cli}";
+          #   in {
+          #     inherit hola;
+          #   };
+          # }
+        ];
+        inherit pkgs;
+      }).neovim;
+  };
+}
---
+++ main/modulos/flake-parts/estetica/SDDM/default.nix
@@ -0,0 +1,31 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.SDDM = {
+    imports = [
+      inputs.silentSDDM.nixosModules.default
+    ];
+    programs.silentSDDM = {
+      enable = true;
+      theme = "rei";
+      backgrounds = {
+        # boring = "${pkgs.gnome-backgrounds}/share/backgrounds/gnome/symbolic-d.png";
+        # borng = ''/nix/store/h49hvwvgqz5dq9ahsdf1632mlssmlwpy-gnome-backgrounds-48.2.1/share/backgrounds/gnome/symbolic-d'';
+        summit = "${./../../../recursos/Summit_no_resize.mp4}";
+      };
+      settings = {
+        "LoginScreen" = {
+          background = "fhffpgfd338gk8qr8l2c0iaj1nn5rn8g-Summit_no_resize.mp4";
+        };
+        "LockScreen" = {
+          background = "fhffpgfd338gk8qr8l2c0iaj1nn5rn8g-Summit_no_resize.mp4";
+        };
+      };
+      profileIcons = {
+        ruiz = "${./../../../recursos/GoldenSlime.jpg}";
+      };
+    };
+  };
+}
