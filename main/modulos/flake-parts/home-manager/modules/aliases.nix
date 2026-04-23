15808b9f4d15b8afce7acca3afbdea8c0fec456b
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-04-23 19:45:00 -0300

    aliases

---
+++ main/modulos/flake-parts/home-manager/modules/aliases.nix
@@ -0,0 +1,19 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.homeModules.aliases = {
+    home.shellAliases = {
+      haziso = "nix run nixpkgs#nixos-generators -- --format iso --flake $NH_FLAKE#portable -o laptop";
+      rephm = "sudo nixos-rebuild switch --flake $NH_FLAKE";
+      rdev = "cd Documentos/Rust/Rust/";
+      ds = "nix-on-droid switch --flake $HOME/nixos/main/";
+      nc = "cd $HOME/nixos/main/ && git pull --no-edit && vi .";
+      ".." = "cd ..";
+      x = "exit";
+      ff = "fastfetch";
+      ns = "nix-shell -p ";
+    };
+  };
+}
