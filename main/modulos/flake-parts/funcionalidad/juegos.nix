cac2c858e684ac21cb71b5525aafeb4e0fd48c09
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-04-23 17:07:46 -0300

    ajhdsjkdsa

---
+++ main/modulos/flake-parts/funcionalidad/juegos.nix
@@ -0,0 +1,60 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.juegos = {pkgs, ...}: {
+    programs.steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+
+    services.xserver.videoDrivers = ["amdgpu"];
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    hardware.amdgpu = {
+      opencl.enable = true;
+      initrd.enable = true;
+      overdrive.enable = true;
+    };
+    services.lact.enable = true;
+
+    # hardware.opengl.driSupport32Bit = true;
+    # hardware.opengl.enable = true;
+    # hardware.pulseaudio.support32Bit = true;
+    environment.systemPackages = with pkgs; [
+      # mangohud
+      # protonup
+
+      #NOTE: rocket league
+      vulkan-tools
+      heroic
+      # lutris
+
+      #NOTE: minecraft
+      prismlauncher
+    ];
+    environment.sessionVariables = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/ruiz/.steam/steam/compatibilitytools.d/LegacyRuntime/compatibilitytool.vdf";
+    };
+  };
+  #NOTE: roblox
+
+  services.flatpak = {
+    enable = true;
+    packages = [
+      "org.vinegarhq.Sober"
+    ];
+    update = {
+      #NOTE: en caso de querer actualizar flatpak en cada rebuild
+      # onActivation = true;
+      auto = {
+        enable = true;
+        onCalendar = "weekly"; # Default value
+      };
+    };
+  };
+}
