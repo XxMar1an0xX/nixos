7331412e5dcd0c96914f3dac29fe7b1c64e3851e
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-06-25 09:48:03 -0300

    arduino dev separation

---
+++ main/flake-parts/custom-pkgs/NVF/arduinoNVF.nix
@@ -0,0 +1,35 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  perSystem = {
+    pkgs,
+    self',
+    ...
+  }: {
+    packages.arduinoNVF =
+      (inputs.nvf.lib.neovimConfiguration {
+        modules = [
+          {_class = "nvf";}
+          # {_module.args = {inherit self';};}
+          self.modules.nvf.funcionalidad
+          self.modules.nvf.theme
+          # self.modules.nvf.rust
+          # self.modules.nvf.nix
+          self.modules.nvf.lspFeatures
+          self.modules.nvf.extraLangs
+          self.modules.nvf.arduino
+
+          # {
+          #   _module.args = let
+          #     hola = "${self.packages.aarch64-linux.arduino-cli}";
+          #   in {
+          #     inherit hola;
+          #   };
+          # }
+        ];
+        inherit pkgs;
+      }).neovim;
+  };
+}
