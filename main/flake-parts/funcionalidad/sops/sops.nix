07b867e7acd0ae621695d5ec82f47d16c6afd191
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-07-29 22:37:15 -0300

    sops gh auth login

--- main/flake-parts/funcionalidad/sops/sops.nix
+++ main/flake-parts/funcionalidad/sops/sops.nix
@@ -3,14 +3,18 @@
   inputs,
   ...
 }: {
-  flake.nixosModules.sops = {...}: {
+  flake.nixosModules.sops = {config, ...}: {
     imports = [inputs.sops-nix.nixosModules.sops];
 
     sops = {
       defaultSopsFile = ./../../../recursos/secrets/secrets.yaml;
       defaultSopsFormat = "yaml";
       age.keyFile = "/home/ruiz/.config/sops/age/keys.txt";
-      secrets.example-key = {};
+      secrets.github_token = {};
+    };
+
+    environment.sessionVariables = {
+      GH_TOKEN = "$(cat ${config.sops.secrets.github_token.path})";
     };
 
     #NOTE: openssh keygen
