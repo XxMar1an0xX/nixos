c799a7b0664b0cb15ea2780de0d504fe996aef9e
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-07-31 20:39:03 -0300

    rustdev andando (?)

--- main/flake-parts/devshells/devRust/default.nix
+++ main/flake-parts/devshells/devRust/default.nix
@@ -1,7 +1,6 @@
 {
   self,
   inputs,
-  # self',
   ...
 }: {
   perSystem = {
@@ -9,18 +8,26 @@
     self',
     ...
   }: {
-    _module.args.pkgs = import inputs.nixpkgs {
-      overlays = [
-        inputs.rust-overlay.overlays.default
-      ];
-      config = {
-        allowUnfree = true;
-      };
-    };
+    #TODO: este es dependiente del overlay definido en arduino-nix
     devShells.devRust = pkgs.mkShell {
       buildInputs = [
         pkgs.rust-bin.stable.latest.default
+        self'.packages.NVF
       ];
+      shellHook =
+        /*
+        bash
+        */
+        ''
+          cd $HOME
+          if ! test -e $HOME/Rust; then
+                  git clone https://github.com/XxMar1an0xX/Rust.git
+          else
+                  cd $HOME/Rust
+                  git pull --no-edit
+          fi
+          cd $HOME/nixos/main/
+        '';
     };
   };
 }
