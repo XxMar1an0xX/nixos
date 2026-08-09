3799936ea2f98da822668040650e9ffbd1c8508d
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-08-09 18:02:10 -0300

    arduinodev update

--- main/flake-parts/devshells/devArduino/default.nix
+++ main/flake-parts/devshells/devArduino/default.nix
@@ -25,11 +25,9 @@
         */
         ''
           mkdir -p $HOME/Arduino/libraries
-          # sudo rm -rf $HOME/Arduino/libraries/*
-          cp -r --symbolic-link --update ${libraryPath}/libraries/* $HOME/Arduino/libraries
+          sudo cp -rf --symbolic-link --update ${libraryPath}/libraries/* $HOME/Arduino/libraries
           mkdir -p $HOME/.arduino15/packages
-          # sudo rm -rf $HOME/.arduino15/packages/*
-          cp -r --symbolic-link --update ${dataPath}/packages/* $HOME/.arduino15/packages
+          sudo cp -rf --symbolic-link --update ${dataPath}/packages/* $HOME/.arduino15/packages
           export ARDUINO="$HOME/Documentos/Arduino/"
         '';
       #TODO: se necesita agregar '--board-options FlashMode=dio' o 'esp32:esp32:esp32:FlashMode=dio' para que ande
