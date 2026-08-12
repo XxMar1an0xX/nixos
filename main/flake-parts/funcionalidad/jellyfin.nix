b63b755791c33282e27ea5c818ef06ddd55623f7
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-08-11 22:12:44 -0300

    no tenia el  nombre que era jellyfin

--- main/flake-parts/funcionalidad/jellyfin.nix
+++ main/flake-parts/funcionalidad/jellyfin.nix
@@ -3,7 +3,9 @@
   inputs,
   ...
 }: {
-  flake.nixosModules.invidious = {pkgs, ...}: {
+  flake.nixosModules.jellyfin = {pkgs, ...}: {
+    #NOTE: jellyfin local host
+    # http://localhost:8096/
     services.jellyfin = {
       enable = true;
       openFirewall = true;
