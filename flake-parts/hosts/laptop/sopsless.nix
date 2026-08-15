cdc164552f9c5332467f3832a6d4be0b3ab90b36
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-08-14 23:09:32 -0300

    laptop sopsless config for install

---
+++ flake-parts/hosts/laptop/sopsless.nix
@@ -0,0 +1,24 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosConfigurations.laptopNoSops = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.configLaptop
+      self.nixosModules.bootMenu
+      self.nixosModules.SDDM
+      self.nixosModules.stylix
+      self.nixosModules.juegos
+      self.nixosModules.hardwareTweaks
+      self.nixosModules.esenciales
+      self.nixosModules.limpieza
+      self.nixosModules.winapps
+      # self.nixosModules.sops
+      self.nixosModules.commonconfig
+      self.nixosModules.invidious
+      self.diskoConfigurations.laptop
+      inputs.nixos-hardware.nixosModules.lenovo-thinkpad-l480
+    ];
+  };
+}
