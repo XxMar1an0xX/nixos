6ff2d0509e975722a0c05c06c69516a29373db4d
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-08-16 13:45:21 -0300

    veamos si con menor funcionalidad anda

--- flake-parts/hosts/laptop/sopsless.nix
+++ flake-parts/hosts/laptop/sopsless.nix
@@ -8,15 +8,15 @@
       self.nixosModules.configLaptop
       self.nixosModules.bootMenu
       self.nixosModules.SDDM
-      self.nixosModules.stylix
-      self.nixosModules.juegos
+      # self.nixosModules.stylix
+      # self.nixosModules.juegos
       self.nixosModules.hardwareTweaks
       self.nixosModules.esenciales
       self.nixosModules.limpieza
-      self.nixosModules.winapps
+      # self.nixosModules.winapps
       # self.nixosModules.sops
       self.nixosModules.commonconfig
-      self.nixosModules.invidious
+      # self.nixosModules.invidious
       self.diskoConfigurations.laptop
       inputs.nixos-hardware.nixosModules.lenovo-thinkpad-l480
     ];
