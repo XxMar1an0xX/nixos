7d25dcf5e224b11ec17adcea11749d5bf17aafc2
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-08-17 20:04:13 -0300

    arduino devshell añadido proogramacion del massa

--- flake-parts/devshells/devArduino/devArduino.nix
+++ flake-parts/devshells/devArduino/devArduino.nix
@@ -28,7 +28,15 @@
           sudo cp -rf --symbolic-link --update ${libraryPath}/libraries/* $HOME/Arduino/libraries
           mkdir -p $HOME/.arduino15/packages
           sudo cp -rf --symbolic-link --update ${dataPath}/packages/* $HOME/.arduino15/packages
-          export ARDUINO="$HOME/Documentos/Arduino/"
+          if ! test -e $HOME/Documentos/ProgramacionMassa2026; then
+                  cd $HOME/Documentos
+                  git clone https://github.com/XxMar1an0xX/ProgramacionMassa2026.git
+          else
+                  cd $HOME/Documentos/ProgramacionMassa2026
+                  git pull --no-edit
+          fi
+          cd $HOME/Documentos/ProgramacionMassa2026
+          export ARDUINO="$HOME/Documentos/ProgramacionMassa2026"
         '';
       #TODO: se necesita agregar '--board-options FlashMode=dio' o 'esp32:esp32:esp32:FlashMode=dio' para que ande
     };
