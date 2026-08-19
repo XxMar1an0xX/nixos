4c4ba4b0f94576975e7625e3631832768c215d1a
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-08-19 13:57:31 -0300

    extensiones es demasiado complicado para mi ahora

--- flake-parts/home-manager/modules/firefox/extensiones.nix
+++ flake-parts/home-manager/modules/firefox/extensiones.nix
@@ -33,6 +33,7 @@
         };
       };
       profiles.ruiz.extensions = {
+        force = true;
         packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
           sponsorblock
           darkreader
@@ -45,16 +46,31 @@
         ];
 
         settings = {
+          #TODO: estos no van a funcionar porque usan un metodo distinto de localizar configuracion que el que usa home manager, efectivamente o es cambiar el codigo fuente o es automatizar la accion de importa configuracion
           "sponsorBlocker@ajay.app".settings = {
-            enables = true;
+            # enabled = true;
             invidiousInstances = [
               "127.0.0.1"
               "inv.nadeko.net"
             ];
             supportInvidious = true;
           };
+          "{d7742d87-e61d-4b78-b8a1-b469842139fa}".settings = {
+            # enabled = true;
+            "searchEngines" = ''
+              w: https://www.wikipedia.org/w/index.php?title=Special:Search&search=%s Wikipedia
+              np: https://search.nixos.org/packages?channel=unstable&query=%s Package Search
+              no: https://search.nixos.org/options?channel=unstable&query=%s Nixos Options
+              hm: https://home-manager-options.extranix.com/?query=%s HomeManager Options
+
+              inv: http://127.0.0.1:3000/search?q=%s Invidious own Instance
+
+              repo: https://github.com/XxMar1an0xX/nixos Repo Nixosa
+            '';
+
+            "settingsVersion" = "2.4.2";
+          };
         };
-        force = true;
       };
       nativeMessagingHosts = [pkgs.keepassxc];
     };
