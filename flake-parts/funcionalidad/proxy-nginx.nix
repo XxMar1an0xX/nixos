987a7d5342cc7463c1d38ea4cc9f356fdf9aa0c6
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-08-20 14:18:23 -0300

    invidious local wlan reverse proxy andando

--- flake-parts/funcionalidad/proxy-nginx.nix
+++ flake-parts/funcionalidad/proxy-nginx.nix
@@ -4,23 +4,33 @@
   ...
 }: {
   flake.nixosModules.caddy = {pkgs, ...}: {
-    networking.firewall.allowedTCPPorts = [80];
+    networking.firewall.allowedTCPPorts = [80 433];
     services.nginx = {
       enable = true;
 
-      recommendedProxySettings = true;
+      #NOTE: por alguna razon romple invidious
+      # recommendedProxySettings = true;
 
       virtualHosts."invidious.local" = {
         listen = [
           {
-            addr = "192.168.1.50";
+            addr = "0.0.0.0";
             port = 80;
           }
         ];
 
         locations."/" = {
           proxyPass = "http://127.0.0.1:3000";
-          proxyWebsockets = true;
+          # proxyWebsockets = true;
+
+          extraConfig = ''
+            # proxy_pass http://127.0.0.1:3000;
+            # Needed for alternative domains to work, check Invidious `config.example.yml`
+            # file to get more details on how it works.
+            proxy_set_header X-Forwarded-Host $host;
+            proxy_http_version 1.1;     # to keep alive
+            proxy_set_header Connection ""; # to keep alive
+          '';
         };
       };
     };
