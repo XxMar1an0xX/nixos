0d5d5a42cc5322a478c2e70f1bdf73cdcb2a1a6c
Xx_Mar1an0_xX <frodoepifanio@protonmail.com>
2026-08-29 20:40:02 -0300

    principal sin sops config

---
+++ flake-parts/hosts/principal/principalsinsops.nix
@@ -0,0 +1,24 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosConfigurations.nixosnosops = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.configPrincipal
+      self.nixosModules.bootMenu
+      self.nixosModules.SDDM
+      self.nixosModules.stylix
+      self.nixosModules.juegos
+      self.nixosModules.hardwareTweaks
+      self.nixosModules.esenciales
+      self.nixosModules.limpieza
+      self.nixosModules.winapps
+      # self.nixosModules.sops
+      self.nixosModules.commonconfig
+      self.nixosModules.invidious
+      self.nixosModules.searxng
+      self.nixosModules.nginx
+    ];
+  };
+}
