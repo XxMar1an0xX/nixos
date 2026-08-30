8acba59c923cbbf80ae651102ad642f1b9364d0e
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-08-30 18:40:00 -0300

    haciendo instancia de searxng publica

--- flake-parts/funcionalidad/proxy-nginx.nix
+++ flake-parts/funcionalidad/proxy-nginx.nix
@@ -33,11 +33,11 @@
           extraConfig = ''
             reverse_proxy 127.0.0.1:3000
           '';
-          #                                         tls {
-          #   issuer acme {
-          #     profile shortlived
-          #   }
-          # }
+        };
+        "searchruiz.duckdns.org" = {
+          extraConfig = ''
+            reverse_proxy 127.0.0.1:5050
+          '';
         };
       };
     };
