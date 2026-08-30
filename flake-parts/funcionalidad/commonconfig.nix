b216e1bf0a3fb03bd01d722c9969e53666d37ef6
XxMar1an0xX <frodoepifanio@prptonmail.com>
2026-08-30 01:03:30 -0300

    esto para que mi repo se auto restableza a cada activacion

--- flake-parts/funcionalidad/commonconfig.nix
+++ flake-parts/funcionalidad/commonconfig.nix
@@ -19,6 +19,21 @@
     qt.enable = true;
     hardware.graphics.enable = true;
 
+    system.activationScripts = {
+      nixRepo = {
+        deps = ["specialfs"];
+        text =
+          /*
+          bash
+          */
+          ''
+            if ! test -e $HOME/nixos; then
+                    git clone https://github.com/XxMar1an0xX/nixos.git $HOME
+            fi
+          '';
+      };
+    };
+
     #NOTE: networking
     networking = {
       firewall.allowedTCPPorts = [53317 8081 22000];
